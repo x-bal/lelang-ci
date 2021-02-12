0abf7a4758720b5fdd25e4279279e5ec8054590c
MXbal <imshroud584@gmail.com>
2021-02-12 22:01:23 +0700

    Update v.1.8

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,27 +5,34 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        // Load model yang dibutuhkan
         $this->load->model('User_M');
         $this->load->model('Masyarakat_M');
         $this->load->library('form_validation');
-        
     }
 
     public function index()
     {
+        // guest adalah helper buatan
         guest();
+
+        // Load view untuk login
         $this->load->view('auth/login');
     }
 
     public function register()
     {
         guest();
+
+        // load view untuk registrasi
         $this->load->view('auth/register');
     }
 
     public function login()
     {
         guest();
+
+
         // Tampung inputan username & password ke dalam variable
         $username = $this->input->post('username', true);
         $password = $this->input->post('password', true);
@@ -37,7 +44,7 @@ class Auth extends CI_Controller
         if ($user) {
             // Buat kondisi apakah password yang dimasukan sesuai dengan password dari database
             if (password_verify($password, $user['password'])) {
-                // Buat session untuk login
+                // Buat session login untuk di kirimkan ke dashboard
                 $this->session->set_userdata([
                     'login' => true,
                     'id_user' => $user['id_user'],
@@ -49,10 +56,12 @@ class Auth extends CI_Controller
                 $this->session->set_flashdata('success', 'Selamat datang kembali');
                 redirect(base_url('dashboard'));
             } else {
+                // Buat pesan jika password salah
                 $this->session->set_flashdata('failed', 'Password salah');
                 redirect(base_url());
             }
-        }else{
+        } else {
+            // Buat pesan jika user tidak ditemukan
             $this->session->set_flashdata('failed', 'Username tidak ditemukan');
             redirect(base_url());
         }
@@ -61,6 +70,8 @@ class Auth extends CI_Controller
     public function signup()
     {
         guest();
+
+        // Buat validasi atau aturan untuk inputan registrasi
         $this->form_validation->set_rules('username', 'Username', 'required|is_unique[users.username]', [
             'required' => 'Username tidak boleh kosong',
             'is_unique' => 'Username tidak tersedia'
@@ -84,9 +95,13 @@ class Auth extends CI_Controller
             'required' => 'No Telp tidak boleh kosong',
         ]);
 
+        // Buat kondisi untuk validasinya
         if ($this->form_validation->run() == false) {
+            // Jika validasinya false atau tidak sesuai maka kembalikan ke method register
             $this->register();
         } else {
+            // Jika validasinya benar maka
+            // Tampung setiap inputan ke dalam sebuah variable
             $dataUser = [
                 'username' => $this->input->post('username', true),
                 'email' => $this->input->post('email', true),
@@ -94,16 +109,20 @@ class Auth extends CI_Controller
                 'level_id' => 3,
             ];
 
+            // Insert data ke table user dengan memanggil modelnya
             $user = $this->User_M->insert($dataUser);
 
+            // Buat inputan untuk dimasukan kedalam table masyarakat
             $data = [
                 'nama' => $this->input->post('nama', true),
                 'telp' => $this->input->post('telp', true),
                 'user_id' => $user
             ];
 
+            // Masukkan ke dalam table masyarakat dengan memanggil model nya
             $this->Masyarakat_M->insert($data);
 
+            // Beri pesan ketika registrasi berhasil
             $this->session->set_flashdata('success', 'Akun berhasil diregistrasi');
             redirect(base_url());
         }
@@ -111,6 +130,7 @@ class Auth extends CI_Controller
 
     public function logout()
     {
+        // Hapus semua session yang kita buat tadi di method login
         session_unset();
         session_destroy();
         redirect(base_url());
--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -5,21 +5,27 @@ class Barang extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        // Load model yang dibutuhkan
         $this->load->model('Barang_M');
         $this->load->library('form_validation');
         $this->load->helper('rupiah_helper');
 
+        // Panggil method auth yang dibuat di helper
         auth();
     }
 
     public function index()
     {
-        guard([1,2]);
+        // Panggil method guard dari helper
+        guard([1, 2]);
+
+        // Buat variable untuk menampung data yang akan kita kirim
         $data = [
             'title' => 'Data Barang',
             'barang' =>  $this->Barang_M->get()
         ];
 
+        // Load view yang akan kita tuju
         $this->load->view('layouts/header', $data);
         $this->load->view('layouts/sidebar', $data);
         $this->load->view('barang/index');
@@ -28,11 +34,14 @@ class Barang extends CI_Controller
 
     public function create()
     {
-        guard([1,2]);
+        guard([1, 2]);
+
+        // Buat variable untuk menampung data yang akan kita kirimkan ke view
         $data = [
             'title' => 'Tambah Barang',
         ];
 
+        // Loag view untuk tambah barang
         $this->load->view('layouts/header', $data);
         $this->load->view('layouts/sidebar', $data);
         $this->load->view('barang/create');
@@ -41,8 +50,9 @@ class Barang extends CI_Controller
 
     public function store()
     {
-        guard([1,2]);
+        guard([1, 2]);
 
+        // buat validasi atau aturan untuk inputan di form tambah barang
         $this->form_validation->set_rules('nama_barang', 'Nama barang', 'required', [
             'required' => 'Nama barang tidak boleh kosong'
         ]);
@@ -55,9 +65,14 @@ class Barang extends CI_Controller
             'required' => 'Deskripsi tidak boleh kosong'
         ]);
 
+        // Buat kondisi untuk validasinya
         if ($this->form_validation->run() == false) {
+            // Jika validasinya bernilai true atau validasinya tidak sesuai, Maka kembalikan ke method create
             $this->create();
         } else {
+            // Jika validasinya benar maka ->
+
+            // Buat config untuk upload gambar barang
             $config = [
                 'allowed_types' => 'jpg|png|jpeg|jfif',
                 'upload_path' => './assets/images/barang',
@@ -65,16 +80,22 @@ class Barang extends CI_Controller
                 'encrypt_name' => true
             ];
 
+            // Load library untuk upload *bawaan ci
             $this->load->library('upload', $config);
 
+            // Buat kondisi apakah inputan images melakukan upload dan sesuai dengan aturan di config
             if ($this->upload->do_upload('images')) {
+                // Jika ada dan sesuai maka tampung nama gambar yang kita upload ke dalam variable image
                 $image = $this->upload->data('file_name');
             } else {
+                // Jika tidak ada dan tidak sesuai, maka tampilkan pesan error nya
                 echo $this->upload->display_errors();
             }
 
+            // Tampung isi dari variable image tadi ke dalam variable imageName
             $imageName = $image;
 
+            // Tampung semua data inputan dari form ke dalam sebuah variable
             $data = [
                 'nama_barang' => $this->input->post('nama_barang', true),
                 'harga_awal' => $this->input->post('harga_awal', true),
@@ -82,35 +103,40 @@ class Barang extends CI_Controller
                 'images' => $imageName
             ];
 
+            // Masukkan data dalam variable data ke table barang dengan memanggil modelnya
             $this->Barang_M->insert($data);
 
+            // Buat pesan untuk notifikasi
             $this->session->set_flashdata('success', 'Barang berhasil ditambahkan');
             redirect(base_url('barang'));
         }
     }
 
-    public function show($id)
-    {
-        $data = [
-            'title' => 'Detail Barang',
-            'barang' => $this->Barang_M->first($id)
-        ];
+    // public function show($id)
+    // {
+    //     $data = [
+    //         'title' => 'Detail Barang',
+    //         'barang' => $this->Barang_M->first($id)
+    //     ];
 
-        $this->load->view('layouts/header', $data);
-        $this->load->view('layouts/sidebar', $data);
-        $this->load->view('barang/show');
-        $this->load->view('layouts/footer');
-    }
+    //     $this->load->view('layouts/header', $data);
+    //     $this->load->view('layouts/sidebar', $data);
+    //     $this->load->view('barang/show');
+    //     $this->load->view('layouts/footer');
+    // }
 
     public function edit($id)
     {
-        guard([1,2]);
+        guard([1, 2]);
 
+        // Tampung data yang akan kita kirim ke dalam sebuah variable
         $data = [
             'title' => 'Edit Barang',
+            // Ambil data barang (1 baris data)
             'barang' => $this->Barang_M->first($id)
         ];
 
+        // Load view untuk edit barang
         $this->load->view('layouts/header', $data);
         $this->load->view('layouts/sidebar', $data);
         $this->load->view('barang/edit');
@@ -119,8 +145,9 @@ class Barang extends CI_Controller
 
     public function update($id)
     {
-        guard([1,2]);
+        guard([1, 2]);
 
+        // Buat validasi aturan untuk inputan edit barang
         $this->form_validation->set_rules('nama_barang', 'Nama barang', 'required', [
             'required' => 'Nama barang tidak boleh kosong'
         ]);
@@ -133,6 +160,7 @@ class Barang extends CI_Controller
             'required' => 'Deskripsi tidak boleh kosong'
         ]);
 
+        // Sama seperti di method store
         if ($this->form_validation->run() == false) {
             $this->create();
         } else {
@@ -145,17 +173,23 @@ class Barang extends CI_Controller
 
             $this->load->library('upload', $config);
 
+            // Karena edit maka kita beri pilihan apakah gambarnya akan diganti atau tidak
             if ($_FILES['images']['name'] == null) {
+                // Jika gambar tidak diganti atau inputan images tidak diisi, maka gunakan gambar lama
                 $image = $this->input->post('image_awal');
             } else {
+                // Jika inputan images di isi maka
                 if ($this->upload->do_upload('images')) {
+                    // Upload gambar baru, tampung namanya ke dalam variable
                     $image = $this->upload->data('file_name');
+                    // Hapus gambar yang lama
                     unlink(FCPATH . 'assets/images/barang/' . $this->input->post('image_awal'));
                 } else {
                     echo $this->upload->display_errors();
                 }
             }
 
+            // Sama dengan method store
             $imageName = $image;
 
             $data = [
@@ -165,8 +199,10 @@ class Barang extends CI_Controller
                 'images' => $imageName
             ];
 
+            // Panggil model update barang
             $this->Barang_M->update($id, $data);
 
+            // Beri pesan notifikasi
             $this->session->set_flashdata('success', 'Barang berhasil diupdate');
             redirect(base_url('barang'));
         }
@@ -174,10 +210,13 @@ class Barang extends CI_Controller
 
     public function destroy($id)
     {
-        guard([1,2]);
-        
+        guard([1, 2]);
+
+        // ambil data barang (1 baris)
         $barang = $this->Barang_M->first($id);
+        // Hapus gambarnya
         unlink(FCPATH . 'assets/images/barang/' . $barang['images']);
+        // Delete data barang nya
         $this->Barang_M->delete($id);
 
         $this->session->set_flashdata('success', 'Barang berhasil dihapus');
